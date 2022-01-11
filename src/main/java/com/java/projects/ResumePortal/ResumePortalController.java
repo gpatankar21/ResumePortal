04a0acd5a37162a9a681bb650607aed355f9c4b9
gpatankar21 <gpatankar21@gmail.com>
2022-01-11 16:02:08 +0530

    Added new resume templates

--- src/main/java/com/java/projects/ResumePortal/ResumePortalController.java
+++ src/main/java/com/java/projects/ResumePortal/ResumePortalController.java
@@ -1,10 +1,13 @@
 package com.java.projects.ResumePortal;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class ResumePortalController {
 
 	@GetMapping("/")
@@ -17,5 +20,12 @@ public class ResumePortalController {
 	public String editResponse()
 	{
 		return "Edit";
+	}
+	
+	@GetMapping("/view/{userId}")
+	public String getProfile(@PathVariable("userId") String userId, Model model)
+	{
+		model.addAttribute(userId);
+		return "resume-templates/template2/index";
 	}
 }
